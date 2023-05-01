64ac9dc32b9928f65f45d812ea3d98d4077ea018
Diego Romero <redgo110.krextro@gmail.com>
2023-05-01 12:57:52 -0300

    working on with spa

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
+import Header from './components/Header';
 import Home from './pages/Home'
 import Sobre from './pages/Sobre'
 import Page404 from './pages/Page404';
@@ -9,6 +10,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
       <Router>
+         <Header />
         <Routes>
           <Route path='/' element={<Home />}/>
           <Route path='/sobre' element={<Sobre />}/>
